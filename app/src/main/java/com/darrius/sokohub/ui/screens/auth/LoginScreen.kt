10d03a3e42799de039fa4f34ddd3ff4779e076ac
kxllswxch <kxlswxch@gmail.com>
2026-04-20 13:24:32 +0300

    initial commit

--- app/src/main/java/com/darrius/sokohub/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/darrius/sokohub/ui/screens/auth/LoginScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -41,6 +42,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.darrius.sokohub.R
+import com.darrius.sokohub.data.AuthViewModel
 import com.darrius.sokohub.navigation.ROUT_HOME
 import com.darrius.sokohub.navigation.ROUT_REGISTER
 import com.darrius.sokohub.ui.theme.neworange
@@ -116,8 +118,14 @@ fun LoginScreen(navController: NavController){
         )
 
         Spacer(modifier = Modifier.height(5.dp))
+
+        val context = LocalContext.current
+        val authViewModel = AuthViewModel(navController, context)
         Button(
-            onClick = {},
+            onClick = {
+                authViewModel.login(email, password)
+
+            },
             colors = ButtonDefaults.buttonColors(neworange),
             shape = RoundedCornerShape(10.dp),
             modifier = Modifier.width(350.dp),
--- app/src/main/java/com/darrius/sokohub/ui/screens/auth/RegisterScreen.kt
+++ app/src/main/java/com/darrius/sokohub/ui/screens/auth/RegisterScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -45,6 +46,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.darrius.sokohub.R
+import com.darrius.sokohub.data.AuthViewModel
 import com.darrius.sokohub.navigation.ROUT_LOGIN
 import com.darrius.sokohub.ui.theme.neworange
 
@@ -152,8 +154,14 @@ fun RegisterScreen(navController: NavController){
 
 
         Spacer(modifier = Modifier.height(5.dp))
+
+        val context = LocalContext.current
+        val authViewModel = AuthViewModel(navController, context)
         Button(
-            onClick = {},
+            onClick = {
+                authViewModel.signup(username, email, password,confirmpassword)
+
+            },
             colors = ButtonDefaults.buttonColors(neworange),
             shape = RoundedCornerShape(10.dp),
             modifier = Modifier.width(350.dp),
